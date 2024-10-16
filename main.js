8ecda7f749bae59810ead81bfabc332e29f53206
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2024-10-16 17:46:58 -0600

    jQuery effects

--- main.js
+++ main.js
@@ -4,14 +4,21 @@ $(document).ready(function () {
   // jQuery Syntax
   // $ is used access jQuery library
   // $("selector").action()
+  // get div element by class name
+  var result = $(".card-body");
+  result.hide();
 
+  //   select panel
+  var panel_1 = $("#panel_1");
+  var panel_2 = $("#panel_2");
+  var panel_3 = $("#panel_3");
+  //   hide panel 2
+  panel_2.hide();
   //   select form using name attribute selector
   $("form[name='signInForm']").on("submit", function (event) {
     // get username by id
     var username = $("#username").val();
 
-    // get div element by class name
-    var result = $(".card-body");
     result.html("<p>Username: " + username + " </p>");
 
     // updating css properties
@@ -21,6 +28,17 @@ $(document).ready(function () {
 
     // set attributes to the element
     result.attr("data-result", "custom value");
+
+    // show the result div
+    // result.show();
+    result.slideDown(1500);
+    // move panel to right
+    panel_1.animate({ right: "100" }, "slow");
+    // fade in panel 2
+    panel_2.fadeIn(1500);
+
+    // toggle between fading in and out
+    panel_3.fadeToggle(1500);
 
     event.preventDefault();
   });
